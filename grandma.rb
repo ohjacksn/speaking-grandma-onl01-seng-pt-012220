30645f7595d1e08d8ba3107e16c1b493064cc24a
Jackson Mo <jacksonmo@me.com>
2020-01-24 18:58:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!" do
+  if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!",
   elsif string == string.upcase
     puts "NO, NOT SINCE 1938!",
